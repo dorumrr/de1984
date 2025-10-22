6c6d71df8083bfbcd9cc1c58cbac03430bd75015
doru <doru.moraru@icloud.com>
2025-10-22 16:36:34 +0100

    chore: Upgrade Android Gradle plugin from 8.2.0 to 8.3.2 and update Java compatibility to version 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,12 +64,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
         freeCompilerArgs += listOf(
             "-Xsuppress-version-warnings",
             // Add reproducible build flags to fix classes.dex differences

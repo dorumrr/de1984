df2db34ad8ec730e94b0e2a6e5f69cd799a281f3
doru <doru.moraru@icloud.com>
2025-10-31 23:45:50 +0000

    Update to v2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         minSdk = 26
         targetSdk = 34
 
-        versionCode = 4
-        versionName = "2.0.0"
+        versionCode = 5
+        versionName = "2.1.0"
 
         vectorDrawables {
             useSupportLibrary = true

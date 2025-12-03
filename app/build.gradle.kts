be16c2ba5e7a739076d042db4ac3cea86749c9c6
doru <doru.moraru@icloud.com>
2025-12-03 00:29:18 +0000

    Update to v2.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         minSdk = 26
         targetSdk = 34
 
-        versionCode = 28
-        versionName = "2.4.9"
+        versionCode = 20
+        versionName = "2.5.0"
 
         // Specify supported languages (reduces APK size by excluding unused translations)
         resourceConfigurations += listOf("en", "ro", "pt", "zh", "it", "fr")
